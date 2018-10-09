6c4ede11bbb22a8249b7f9624cc2fd8e590fc9f3
Rockheung <rockheung@gmail.com>
2018-10-09 15:10:25 +0900

    update psd

--- modules/test_pytoshop.py
+++ modules/test_pytoshop.py
@@ -1,53 +1,61 @@
 import pytoshop
 import numpy as np
+from PIL import Image
 from pytoshop.user import nested_layers as nl
-from pytoshop import enums
+from pytoshop.enums import *
+from pytoshop.image_data import ImageData
+
+
+img = Image.open('hello.png')
+img = np.array(img)
+channels = [img[:,:,0],
+            img[:,:,1],
+            img[:,:,2]]
+data = ImageData(channels=img)
+
 
 img_layer_conf = dict(
-    name = 'layer',
+    name = 'layer_added',
     visible = True,
     opacity = 255,
     group_id = 0,
-    blend_mode = enums.BlendMode.normal,
+    blend_mode = BlendMode.normal,
     top = 0,
     left = 0,
-    bottom = None,
-    right = None,
-    channels = None,
+    bottom = 240,
+    right = 240,
+    channels = channels,
     metadata = None,
     layer_color = 0,
-    color_mode = enums.ColorMode.rgb)
+    color_mode = None)
+
 
 layers_to_psd_conf = dict(
-    color_mode = enums.ColorMode.rgb,
-    version = enums.Version.psd,
-    compression = enums.Compression.rle,
-    depth = enums.ColorDepth.depth8,
+    color_mode = ColorMode.rgb,
+    version = Version.psd,
+    compression = Compression.rle,
+    depth = ColorDepth.depth8,
     size = None,
     vector_mask = False)
 
-color_sheet = np.array([(34,2,5,3,46,63,64,56,7,8),
-                        (34,2,5,3,46,63,64,56,7,8)])
 
 if __name__ == "__main__":
-    with open('../../empty.psd', 'rb') as fd:
+    PSDFILE = 'example.psd'
+    with open(PSDFILE, 'rb') as fd:
         psd = pytoshop.read(fd)
 
-    nested_stuff = nl.psd_to_nested_layers(psd)
-    new_layer = nl.Image(**img_layer_conf)
-    new_layer.set_channel(color=enums.ColorChannel.red,
-                          channel=color_sheet)
-    new_layer.set_channel(color=enums.ColorChannel.green,
-                          channel=color_sheet)
-    new_layer.set_channel(color=enums.ColorChannel.blue,
-                          channel=color_sheet)
-    nested_stuff.append(new_layer)
+        nested_stuff = nl.psd_to_nested_layers(psd)
+        new_layer = nl.Image(**img_layer_conf)
+        nested_stuff.insert(0, new_layer)
 
-    nl.pprint_layers(nested_stuff)
+        nl.pprint_layers(nested_stuff)
 
-    nl.nested_layers_to_psd(psd, **layers_to_psd_conf)
-    with open('../../empty_layerd.psd', 'wb') as fe:
-        psd.write(fe)
+        psd_tunned = nl.nested_layers_to_psd(layers = nested_stuff,
+                                             color_mode = ColorMode.rgb,
+                                             size = (240, 240))
+
+        with open(PSDFILE.split('.')[0] + '_tunned.psd', 'wb') as fe:
+            psd_tunned.write(fe)
 
 
 
